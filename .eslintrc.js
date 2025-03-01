2c36f42919e8b42196c9f91869f7569bc7534749
Parth Sinha <sinhaparth555@gmail.com>
2025-03-01 21:45:35 +0000

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,58 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     },
-    rules: {
-      // Customize rules as needed
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+    'import',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'import/order': ['error', {
+      'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+      'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
+    }],
+    'jsx-a11y/anchor-is-valid': ['error', {
+      'components': ['Link'],
+      'specialLink': ['to']
+    }],
+    'no-console': ['warn', { 'allow': ['warn', 'error'] }]
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': {}
+    },
+    'jsx-a11y': {
+      'components': {
+        'Input': 'input',
+        'Button': 'button'
+      }
     }
-  };
+  }
+}
