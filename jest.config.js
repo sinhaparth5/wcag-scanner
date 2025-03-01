2e1e031abebe96e0351c8e3eb5685d5aa92eab9b
Parth Sinha <sinhaparth555@gmail.com>
2025-03-01 22:30:48 +0000

    updated everythin

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    testMatch: ['**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js', 'json'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/test/**/*.{ts,tsx}'
+  ],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+};
